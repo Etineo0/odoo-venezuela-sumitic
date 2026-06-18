869a0b02da0f6548cd99a2008cf1225a8df285b0
Etineo0 <sumitic2205ca@gmail.com>
2026-06-17 23:14:06 -0400

    Update template_ve.py

--- l10n_ve_chart/models/template_ve.py
+++ l10n_ve_chart/models/template_ve.py
@@ -11,18 +11,19 @@ class AccountChartTemplate(models.AbstractModel):
             "name": _("Venezuela"),
             "code_digits": "7",
             "account_account": {
-                # CUENTA OBLIGATORIA PARA EVITAR EL KEYERROR
-                "account_110104": {
+                "sumitic_ve_110104": {
                     "name": "PAGOS PENDIENTES POR CONCILIAR",
                     "code": "110104",
                     "account_type": "asset_current", 
                     "reconcile": True,
                 },
-                # Tus otras cuentas...
                 "sumitic_ve_110301": {"name": "CLIENTES NACIONALES BS", "code": "110301", "account_type": "asset_receivable", "reconcile": True},
                 "sumitic_ve_210101": {"name": "PROVEEDORES NACIONALES BS", "code": "210101", "account_type": "liability_payable", "reconcile": True},
                 "sumitic_ve_410101": {"name": "VENTAS NACIONALES", "code": "410101", "account_type": "income", "reconcile": False},
                 "sumitic_ve_510101": {"name": "COMPRAS NACIONALES", "code": "510101", "account_type": "expense", "reconcile": False},
+            },
+            "account_tax_group": {
+                "tax_group_iva_16": {"name": "IVA 16%"},
             },
             "property_account_receivable_id": "sumitic_ve_110301",
             "property_account_payable_id": "sumitic_ve_210101",
@@ -37,6 +38,5 @@ class AccountChartTemplate(models.AbstractModel):
                 "account_fiscal_country_id": False,
                 "bank_account_code_prefix": "1113",
                 "cash_account_code_prefix": "1111",
-                "transfer_account_code_prefix": "1129003",
             },
         }
