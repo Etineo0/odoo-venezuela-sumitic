6b194448a11b4b2144909d6151f30aca9e667420
Etineo0 <sumitic2205ca@gmail.com>
2026-06-17 23:26:41 -0400

    Update template_ve.py

--- l10n_ve_chart/models/template_ve.py
+++ l10n_ve_chart/models/template_ve.py
@@ -6,20 +6,33 @@ class AccountChartTemplate(models.AbstractModel):
 
     @template("ve")
     def _get_ve_template_data(self):
-        # Esta estructura es la que espera el motor contable de Odoo
         return {
             "name": _("Venezuela"),
             "code_digits": "7",
-            "account_account": {
+            # Es vital que 'account.account' sea la clave principal de los datos
+            "account.account": {
                 "sumitic_ve_110104": {
                     "name": "PAGOS PENDIENTES POR CONCILIAR",
                     "code": "110104",
-                    "account_type": "asset_current", # REQUERIDO POR ENTERPRISE
+                    "account_type": "asset_current", 
                     "reconcile": True,
                 },
-                "sumitic_ve_110301": {"name": "CLIENTES NACIONALES BS", "code": "110301", "account_type": "asset_receivable", "reconcile": True},
-                "sumitic_ve_210101": {"name": "PROVEEDORES NACIONALES BS", "code": "210101", "account_type": "liability_payable", "reconcile": True},
+                "sumitic_ve_110301": {
+                    "name": "CLIENTES NACIONALES BS", 
+                    "code": "110301", 
+                    "account_type": "asset_receivable", 
+                    "reconcile": True
+                },
             },
             "property_account_receivable_id": "sumitic_ve_110301",
             "property_account_payable_id": "sumitic_ve_210101",
+        }
+
+    @template("ve", "res.company")
+    def _get_ve_res_company(self):
+        return {
+            self.env.company.id: {
+                "account_fiscal_country_id": False,
+                "bank_account_code_prefix": "1113",
+            },
         }
